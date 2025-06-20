2bee0e845daae27aae1c1d2906fbe1e4fc5b4225
ann2005-m <annbizcreat23@gmail.com>
2025-06-20 18:09:38 +0200

    Update navbar.component.ts

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -61,6 +61,7 @@ library.add(faLanguage, faSearch, faSignInAlt, faSignOutAlt, faComment, faBomb, 
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   imports: [
